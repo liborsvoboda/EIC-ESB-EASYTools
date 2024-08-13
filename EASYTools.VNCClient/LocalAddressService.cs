bf6c712bf7a71dee3295341167054f4493b24677
Libor Svoboda <libor.svoboda@kliknezde.cz>
2024-08-13 02:46:35 +0200

    First Upload

---
+++ EASYTools.VNCClient/LocalAddressService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace RemoteDesktopVNCClientApp
+{
+    class LocalAddressService
+    {
+        public List<IPAddress> GetLocalIpAddress()
+        {
+            List<IPAddress> localAddress = new List<IPAddress>();
+
+            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+
+            foreach (NetworkInterface adapter in adapters)
+            {
+                if (adapter.OperationalStatus.Equals(OperationalStatus.Up))
+                {
+                    IPInterfaceProperties properties = adapter.GetIPProperties();
+                    foreach (IPAddressInformation ipInfo in properties.UnicastAddresses)
+                    {
+                        localAddress.Add(ipInfo.Address);
+                    }
+                }
+            }
+
+            return localAddress;
+        }
+    }
+}
